32ea42eed4fa46e74c2b5eeaa40d42556b14f0a4
andresmtr <andresthrash@hotmail.com>
2022-07-29 08:30:35 -0500

    Update grammar

--- BasePortfolio/static/base/js/scroll.js
+++ BasePortfolio/static/base/js/scroll.js
@@ -7,20 +7,20 @@ window.sr = ScrollReveal();
 
 window.srPortada = ScrollReveal();
         srPortada.reveal('.gridBanner',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
         // distance: '450px'
     });
 
     srPortada.reveal('.moveWordPortada',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         delay: 500,
         // distance: '150%'
     });
 
     srPortada.reveal('.moveWordTyped',{
-        duration: 2000,
+        duration: 3500,
         origin: 'rigth',
         // delay: 500,
         distance: '150%'
@@ -28,81 +28,81 @@ window.srPortada = ScrollReveal();
 
 window.srAcercaPage = ScrollReveal();
     srAcercaPage.reveal('.titleAbout',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
     });
 
     srAcercaPage.reveal('.subtitleAbout',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.ParaAbout',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.pictureHistory',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.subtitlehistory',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
     srAcercaPage.reveal('.paraHistory',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.fullStackDevelop',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.DataScience',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
     srAcercaPage.reveal('.Office',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
     srAcercaPage.reveal('.Coding',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.jobs',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
 
     srAcercaPage.reveal('.projectsAnimation',{
-        duration: 3000,
+        duration: 3500,
         origin: 'rigth',
         distance: '450px'
     });
     srAcercaPage.reveal('.tutorialsAnimation',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
         distance: '450px'
     });
 
 window.srProjectPage = ScrollReveal();
     srProjectPage.reveal('.gridProjects',{
-        duration: 3000,
+        duration: 3500,
         origin: 'left',
     });
 
